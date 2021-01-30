8b1e35df3739d9bfdb5180db660c543debad1d87
Mohammad Tanviruzzaman <motanv@system76-pc.localdomain>
2021-01-30 03:28:50 -0800

    input

---
+++ python/2015_python_crash_course/chapter_07_user_input_while_loop/input.py
@@ -0,0 +1,20 @@
+#message = input( "Tell me something and I shall repeat it back to you!\n" )
+#print( message )
+
+#name = input( "Please enter your name: " )
+#print( "Hello, " + name + "!" )
+
+# prompt = "How old are you? "
+# age = input( prompt )
+# age = int(age)
+
+# if age >= 18:
+	# print("You are eligible to vote.")
+# elif age < 18:
+	# print("You are ineligible to vote.")
+
+# print( "Bye for now!" )
+
+# Interesting that in Python -7 % 3 = 2, but in Java -7 % 3 = -1
+# Python: -7 = -3*3 + 2
+# Java  : -7 = -2*3 - 1
