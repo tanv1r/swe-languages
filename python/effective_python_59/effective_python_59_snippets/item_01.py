4fb93afa8f108610ea5937a1029fffd8b327ab03
Mohammad Tanviruzzaman <motanv@system76-pc.localdomain>
2021-02-01 20:06:16 -0800

    hola

--- python/effective_python_59/effective_python_59_snippets/item_01.py
+++ python/effective_python_59/effective_python_59_snippets/item_01.py
@@ -1,3 +1,13 @@
+# There are two major versions of Python in use: 2 and 3
+# Know your python version
+
 import sys
+
 print(sys.version_info)
 print(sys.version)
+
+# Output from my vscode in ubuntu
+#
+# sys.version_info(major=3, minor=8, micro=5, releaselevel='final', serial=0)
+# 3.8.5 (default, Jul 28 2020, 12:59:40) 
+# [GCC 9.3.0]
