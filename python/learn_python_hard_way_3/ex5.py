9330a95a5200684f985ad549d6ad862dcb02ebc4
root <root@system76-pc.localdomain>
2021-03-07 01:19:13 -0800

    hola

---
+++ python/learn_python_hard_way_3/ex5.py
@@ -0,0 +1,17 @@
+name = 'Tanvir Zaman'
+age_year = 39
+height_inch = 70
+weight_lb = 140
+eye_color = 'black'
+teeth_color = 'white'
+hair_color = 'black'
+
+print(f"Let's talk about { name}.")
+print(f"He's { height_inch} inches tall.")
+print(f"He's { weight_lb} pounds heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got { eye_color} eyes and { hair_color} hair.")
+print(f"His teeth are usually { teeth_color} depending on the coffee.")
+
+total =  age_year +  height_inch +  weight_lb
+print(f"If I add { age_year}, { height_inch}, and { weight_lb} I get {total}.")
