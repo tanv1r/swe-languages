8ec8afd7928ff623d1c96b918a7c00de096824ee
Mohammad Tanviruzzaman <motanv@system76-pc.localdomain>
2021-04-06 12:36:31 -0700

    blah

--- cpp/ppp_using_cpp_bs/ch02_hello_world/helloworld.cpp
+++
@@ -1,9 +0,0 @@
-// This program outputs the message "Hello, World!" to the monitor
-
-#include "../std_lib_facilities.h"
-
-int main() {
-    cout << "Hello, World!\n";
-    keep_window_open();
-    return 0;
-}
--- cpp/ppp_using_cpp_bs/ch03_obj_typ_val/03_try_this.cpp
+++
@@ -1,7 +0,0 @@
-#include "../std_lib_facilities.h"
-
-int main() {
-    int x = -3;
-    int y = 5;
-    cout << x % y << endl;
-}
